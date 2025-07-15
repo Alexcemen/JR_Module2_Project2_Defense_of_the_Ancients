dcc065ebeff98d7f71de703dc007cda52b593c45
Semen Rybkin <alexcemen99@gmail.com>
2025-07-15 08:40:51 +0700

    Почистил GameSolution

--- src/main/java/org/example/app/GameSolution.java
+++ src/main/java/org/example/app/GameSolution.java
@@ -1,7 +1,5 @@
 package org.example.app;
 
-
-import com.javarush.engine.cell.Color;
 import com.javarush.engine.cell.Game;
 import org.example.command.AnimalAttacker;
 import org.example.command.AnimalMover;
@@ -16,53 +14,63 @@ import org.example.data.ValueCells;
 import org.example.entities.animals.*;
 import org.example.fabrics.ExecutorsFabric;
 import org.example.fabrics.RuneFabric;
-import org.example.models.Faction;
-import org.example.util.Field;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.List;
 import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
 public class GameSolution extends Game {
-    private final static Logger log = LoggerFactory.getLogger(GameSolution.class);
-    private int SIDE;
-    private Config config;
-    private AnimalsFabric animalsFabric;
-    private List<AbstractAnimal> animals;
-    private AnimalMover animalMover;
-    private AnimalAttacker animalAttacker;
-    private ValueCells valueCells;
-    private RuneFabric runeFabric;
-    private List<AbstractRune> runes;
-    private ExecutorsFabric executorsFabric;
+    private VictoryChecker victoryChecker;
     private AnimalsExecutor animalsExecutor;
     private RunesExecutor runesExecutor;
     private DrawExecutor drawExecutor;
-    private VictoryChecker victoryChecker;
-
-    private ScheduledFuture runesLogicExecutor;
     private ScheduledFuture animalLogicExecutor;
+    private ScheduledFuture runesLogicExecutor;
     private ScheduledFuture drawLogicExecutor;
 
     @Override
     public void initialize() {
-        config = Config.load();
-        SIDE = config.sideField;
-        animalsFabric = new AnimalsFabric(config);
-        animals = animalsFabric.getAnimalsList();
-        animalMover = new AnimalMover(config, animals);
-        animalAttacker = new AnimalAttacker(animals);
-        valueCells = new ValueCells(config);
-        runeFabric = new RuneFabric(config, animals);
-        executorsFabric = new ExecutorsFabric();
-        runes = runeFabric.getRunes();
+        Config config = Config.load();
+        //Field
+        int SIDE = config.sideField;
         setScreenSize(SIDE, SIDE);
-        animalsExecutor = new AnimalsExecutor(executorsFabric, animals, runes, animalMover, animalAttacker);
-        runesExecutor = new RunesExecutor(executorsFabric, runeFabric);
-        drawExecutor = new DrawExecutor(executorsFabric, this, animals, runes, SIDE, valueCells);
-        victoryChecker = new VictoryChecker(animals, executorsFabric, this, runesLogicExecutor, animalLogicExecutor, drawLogicExecutor);
+        //Animals
+        AnimalsFabric animalsFabric = new AnimalsFabric(config);
+        List<AbstractAnimal> animals = animalsFabric.getAnimalsList();
+        AnimalMover animalMover = new AnimalMover(config, animals);
+        AnimalAttacker animalAttacker = new AnimalAttacker(animals);
+        //Runes
+        RuneFabric runeFabric = new RuneFabric(config, animals);
+        List<AbstractRune> runes = runeFabric.getRunes();
+        //Draw
+        ValueCells valueCells = new ValueCells(config);
+        //Executors
+        ExecutorsFabric executorsFabric = new ExecutorsFabric();
+        animalsExecutor = new AnimalsExecutor(
+                executorsFabric,
+                animals,
+                runes,
+                animalMover,
+                animalAttacker
+        );
+        runesExecutor = new RunesExecutor(
+                executorsFabric,
+                runeFabric
+        );
+        drawExecutor = new DrawExecutor(
+                executorsFabric,
+                this,
+                animals,
+                runes,
+                SIDE,
+                valueCells
+        );
+        victoryChecker = new VictoryChecker(
+                animals,
+                executorsFabric,
+                this,
+                runesLogicExecutor,
+                animalLogicExecutor,
+                drawLogicExecutor);
+
         startGame();
     }
 
